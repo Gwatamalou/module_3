3e0f4b80d27f86a1514e41f1e0d2136ec18d60fe
sdasr <asd@mail.ru>
2024-06-09 19:17:09 +0600

    answer

--- module_2_6.py
+++ module_2_6.py
@@ -1,1 +1,8 @@
-asdasdasda
+def _single_root_words(root_word, *other_word):
+    same_word = []
+    for i in other_word:
+        if i.lower().count(root_word.lower()) or root_word.lower().count(i.lower()):
+            same_word.append(i)
+    return same_word
+
+print(_single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
